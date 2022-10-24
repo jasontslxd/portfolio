ffa560ae26ee096708cd7e9273336619c6f3b5c1
jasontslxd <ckyjjt@gmail.com>
2022-10-24 22:03:58 +1100

    clean commit

--- src/components/yt-clone/apis/YouTube.js
+++ src/components/yt-clone/apis/YouTube.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const KEY = process.env.REACT_APP_API_KEY;
-console.log(process.env)
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
