6fee0623c3b6b1f574c58f86cbf61bca1c8ad629
jasontslxd <ckyjjt@gmail.com>
2022-10-24 13:43:18 +1100

    added PQWGAN samples and captions

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './Projects.css';
 import shadowdefend from '../assets/shadowdefend.png';
 import pqwgan from '../assets/pqwgan.png';
+import pqwgan_samples from '../assets/pqwgan_samples.png'
 
 function Projects() {
   return (
@@ -20,13 +21,23 @@ function Projects() {
 
           <h3>PQWGAN</h3>
           <p>
-            Code written in python to run the quantum circuit simulations for my thesis.
-            The github repository is found <a className="link" href="https://github.com/jasontslxd/PQWGAN" 
+            Code written in python to run the quantum circuit simulations for my thesis on 
+            quantum generative adversarial networks. The github repository is found <a className="link" href="https://github.com/jasontslxd/PQWGAN" 
             target="blank">here</a>.
           </p>
-          <a href="https://github.com/jasontslxd/PQWGAN" target="blank">
-            <img class="image" src={pqwgan} alt="pqwgan"/>
-          </a>
+          <figure>
+            <a href="https://github.com/jasontslxd/PQWGAN" target="blank">
+              <img class="image" src={pqwgan} alt="pqwgan circuit"/>
+            </a>
+            <figcaption className="caption">Quantum circuit for the quantum generator.</figcaption>
+          </figure>
+
+          <figure>
+            <a href="https://github.com/jasontslxd/PQWGAN" target="blank">
+              <img class="image" src={pqwgan_samples} alt="pqwgan outputs"/>
+            </a>
+            <figcaption className="caption">Sample outputs from the PQWGAN framework compared to WGAN-GP and real outputs.</figcaption>
+          </figure>
 
           <h3>ShadowDefend</h3>
           <p>
@@ -35,11 +46,15 @@ function Projects() {
             oriented programming and software design in that project. The github repository is found&nbsp;
             <a className="link" href="https://github.com/jasontslxd/ShadowDefend" target="blank">here</a>.
           </p>
-          <a href="https://github.com/jasontslxd/ShadowDefend" target="blank">
-            <img class="image" src={shadowdefend} alt="shadowdefend"/>
-          </a>
+          <figure>
+            <a href="https://github.com/jasontslxd/ShadowDefend" target="blank">
+              <img class="image" src={shadowdefend} alt="shadowdefend"/>
+            </a>
+            <figcaption className="caption">Screenshot from the game.</figcaption>
+          </figure>
         </div>
       </div>
+      <br />
     </div>
   );
 }
